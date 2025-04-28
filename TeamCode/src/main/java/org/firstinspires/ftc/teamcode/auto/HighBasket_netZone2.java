87f7b441d416b2c030417ca1fb10fd512de12171
qihua <doraqwu11@gmail.com>
2025-04-27 18:09:49 -0700

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/HighBasket_netZone2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/HighBasket_netZone2.java
@@ -82,7 +82,7 @@ public class HighBasket_netZone2 extends LinearOpMode {
             forwardTicks = 400; //rotate counter clockwise
             driveMotors(forwardTicks,forwardTicks,-forwardTicks,-forwardTicks, 0.5, false, robot.yaw0);
             sleep (1500);
-
+//
            forwardTicks = -900; // strafe rt
             driveMotors(forwardTicks,-forwardTicks,-forwardTicks,forwardTicks, 0.5, false, robot.yaw0);
             sleep (700);
